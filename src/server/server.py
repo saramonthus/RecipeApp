f15eb64b65be681cb7797e497da785ac975809a9
saramonthus <saramonthus@gmail.com>
2020-01-14 21:25:48 +0100

    Update server.py
    Unindented an if statement.

--- src/server/server.py
+++ src/server/server.py
@@ -3,15 +3,33 @@ import socketserver
 import requests
 import json
 
+# class raii(object):
+#     """docstring for raii."""
+#
+#     def __init__(self):
+#         print("construct")
+#
+#     def __enter__(self):
+#         print("open")
+#
+#     def __exit__(self, value, type, traceback):
+#         print("close")
+#
+# def raii_open():
+#     return raii()
+#
+# with raii_open() as r:
+#     print("inside with")
+
 def some_function():
     print("some_function got called")
     filename = "../database/JSON.json"
     with open(filename, 'r') as f:
         datastore = json.load(f)
-        if datastore:
-            #add recipe
-            with open(filename, 'w') as f:
-                json.dump(datastore, f)
+    if datastore:
+        #add recipe
+        with open(filename, 'w') as f:
+            json.dump(datastore, f)
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
